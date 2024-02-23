2e4caf01a52acc0a4a8cd21d8c2943dc53009a9e
sssk1led <sssk1led@gmail.com>
2024-02-24 00:46:03 +0300

    своровал стиль в ВК + сделал генерацию списка музыки

---
+++ static/music-list-script.js
@@ -0,0 +1,28 @@
+const music_list = document.querySelector('.music-list') 
+
+function createMusicTrack(music_track_src, music_track_name, music_track_artist, music_track_id) {
+  return`
+    <li class="music-track theme">
+      <button id="button_${music_track_id}" class="play-pause list-content"><i class="fa-solid fa-pause"></i></button>
+      <div class="track-info">
+        <h3 class="music-name list-content">${music_track_name}</h3>
+        <h4 class="music-artist list-content">${music_track_artist}</h4>
+      </div>
+      <audio id="music_${music_track_id}" src="./music/${music_track_src}"></audio>
+    </li>
+    `
+}
+
+const XHR = new XMLHttpRequest()
+XHR.open('GET', 'http://localhost:1488/music_list')
+XHR.responseType = 'json'
+XHR.send()
+XHR.onload = () => {
+  XHR.response.map((music, music_track_id) => {
+    const massOfTrackInfo = music.split('-').filter(info => info != '')
+    const music_track_src = music
+    const music_track_name = massOfTrackInfo[1]
+    const music_track_artist = massOfTrackInfo[0]
+    music_list.insertAdjacentHTML('afterbegin', createMusicTrack(music_track_src, music_track_name, music_track_artist, music_track_id))
+  })
+}
