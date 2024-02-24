109d015094d00529e885f9a21553b93bd2f7b556
sssk1led <sssk1led@gmail.com>
2024-02-24 15:01:23 +0300

    какой же я генич, как же я задолбался работать над этим nav баром!!!! слава богу сделал

--- static/music-list-controls-script1.js
+++ static/music-list-controls-script1.js
@@ -6,24 +6,27 @@ function clickOnButtonTrack(id) {
   const name_id = document.querySelector(`#name_${id}`)
   const artist_id = document.querySelector(`#artist_${id}`)
   const button_sync = document.querySelector('#button_sync')
-
-  stopedAllSound(music_id)
-  musicPlayPause(music_id, button_id)
+  document.querySelectorAll('audio').forEach(element => element.id !== `music_${id}` ? element.classList.remove('sync') : 0 )
+  music_id.classList.add('sync')
+  button_sync.addEventListener('click', () => musicPlayPause(music_id, button_id))
+  stopedAllSound(document.querySelector('.sync'))
+  musicPlayPause(music_id, button_id, name_id, artist_id, id)
 }
 
 function musicPlayPause(music_id, button_id) {
-  const button_sync = document.querySelector('#button_sync')
   if (music_id.paused){
     music_id.play()
-    button_sync.innerHTML = '<i class="fa-solid fa-pause"></i>'
     button_id.innerHTML = '<i class="fa-solid fa-pause"></i>'
   } else {
     music_id.pause()
-    button_sync.innerHTML = '<i class="fa-solid fa-play"></i>'
-    button_id.innerHTML = '<i class="fa-solid fa-play"></i>'
   }
 }
 
+function sync() {
+  const music_id = document.querySelector('audio.sync').id
+  const button_id = document.querySelector('button.sync').id
+  musicPlayPause(music_id, button_id)
+}
 
 function stopedAllSound(music_id) {
   document.querySelectorAll('.play-pause').forEach(element => element.innerHTML = '<i class="fa-solid fa-play"></i>')
@@ -38,15 +41,10 @@ function stopedAllSound(music_id) {
 function setPlayedTrack(id) {
   const name_id = document.querySelector(`#name_${id}`)
   const artist_id = document.querySelector(`#artist_${id}`)
-
   const name = document.querySelector(`#name`)
   const artist = document.querySelector(`#artist`)
-  
-  const button_sync = document.querySelector('#button_sync')
-  const music_id = document.querySelector(`#music_${id}`)
-  const button_id = document.querySelector(`#button_${id}`)
-  
-  button_sync.onclick = () => musicPlayPause(music_id, button_id)
+
   name.innerHTML = name_id.textContent
   artist.innerHTML = artist_id.textContent
+  
 }
